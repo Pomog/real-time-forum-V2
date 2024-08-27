98ec42b933c8859b2f1197591487e3dbfe0b4c20
rjuhkam <robert.juhkam7@gmail.com>
2024-08-27 19:05:44 +0300

    some errors strings fixed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+
 	"github.com/Pomog/real-time-forum-V2/envloader"
 	"github.com/Pomog/real-time-forum-V2/internal/app"
 )
 
+// Function for geting commands from terminal to start API server
 func main() {
 	configPath := flag.String("config-path", "./configs/config.json", "Path to the config file")
 	flag.Parse()
